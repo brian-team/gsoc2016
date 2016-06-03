331426fc976b5a468e7d797817ac5928e2fa7138
dokato <raymon92@gmail.com>
2016-06-03 11:50:33 +0200

    lems units of type per_sec now supported as well

--- supporting.py
+++ supporting.py
@@ -5,6 +5,7 @@ import re
 
 name_to_unit = {u.dispname: u for u in all_units}
 
+
 def from_string(rep):
     """
     Returns `Quantity` object from text representation of a value.
@@ -29,20 +30,32 @@ def from_string(rep):
     # match unit
     m = re.match(' ?([a-zA-Z]+)', rep)
     unit = None
+    per = None
     if m:
         unit = rep[0:m.end()].strip()
+        # special case with per
+        if unit=='per':
+            mper = re.match(' ?per_([a-zA-Z]+)', rep)
+            per = rep[0:mper.end()].strip()[4:]
+            m = mper
         rep = rep[m.end():]
     # match exponent
     m = re.match('-?([0-9]+)?', rep)
     exponent = None
     if len(rep) > 0 and m:
         exponent = rep[0:m.end()]
-    if value and unit and exponent:
-        return float(value) * name_to_unit[unit]**float(exponent)
-    elif value and unit:
-        return float(value) * name_to_unit[unit]
+    if unit:
+        if per:
+            b2unit = 1. / name_to_unit[per]
+        else:
+            b2unit = name_to_unit[unit]
+        if value and exponent:
+            return float(value) * b2unit**float(exponent)
+        elif value:
+            return float(value) * b2unit
     else:
         return float(value)
+
 
 def read_lems_dims(nmlcdpath=""):
     """
@@ -75,11 +88,12 @@ def read_lems_dims(nmlcdpath=""):
         lems_dimensions[name_] = get_or_create_dimension(tmpdim_)
     return lems_dimensions
 
+
 def read_lems_units(nmlcdpath=""):
     """
     Read from 'NeuroMLCoreDimensions.xml' all supported by LEMS
     units.
-    
+
     Parameters
     ----------
     nmlcdpath : `str`, optional
@@ -91,14 +105,14 @@ def read_lems_units(nmlcdpath=""):
         List with LEMS units.
     """
     path = nmlcdpath + "NeuroMLCoreDimensions.xml"
-    domtree = parse("NeuroMLCoreDimensions.xml")
+    domtree = parse(path)
     collection = domtree.documentElement
     unitsCollection = collection.getElementsByTagName("Unit")
     lems_units = []
     for uc in unitsCollection:
         if uc.hasAttribute('symbol'):
             lems_units.append(uc.getAttribute('symbol'))
-            
+
     return lems_units
 
 if __name__ == '__main__':
@@ -106,7 +120,7 @@ if __name__ == '__main__':
     from brian2 import *
     testlist = ["1 mV", "1.234mV", "1.2e-4 mV", "1.23e-5A", "1.23e4A",
                 "1.45E-8 m", "1.23E-8m2", "60", "6000", "123000",
-                "-1.00000008E8"]
+                "-1.00000008E8", "0.07 per_ms", "10pS"]
     for i in testlist:
         from_string(i)
     print 'ok'
