8054847e2f8978f67bc97308c5441251c5e41f22
dokato <raymon92@gmail.com>
2016-08-04 16:42:15 +0200

    tests of recordings updated

---
+++ tests/simulation1_lif.py
@@ -0,0 +1,46 @@
+from brian2 import *
+from brian2lems.lemsexport import all_devices
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='Set simulation flag')
+parser.add_argument("-d", "--device", action="store_true",
+                    help="runs LEMS output building")
+parser.add_argument('--recidx', nargs='+', type=int)
+
+args = parser.parse_args()
+flag_device = args.device
+if flag_device:
+    set_device('lemsdevice')
+if args.recidx:
+    rec_idx = args.recidx
+else:
+    rec_idx = [2, 63]
+
+n = 100
+duration = 1*second
+tau = 10*ms
+
+eqs = '''
+dv/dt = (v0 - v) / tau : volt (unless refractory)
+v0 : volt
+'''
+group = NeuronGroup(n, eqs, threshold='v > 10*mV', reset='v = 0*mV',
+                    refractory=5*ms, method='linear')
+group.v = 0*mV
+group.v0 = '20*mV * i / (N-1)'
+
+statemonitor = StateMonitor(group, 'v', record=rec_idx)
+spikemonitor = SpikeMonitor(group, record=rec_idx)
+
+if flag_device:
+    run(duration)
+    device.build("ifcgmtest.xml")
+else:
+    run(duration)
+    recvec = []
+    for ri in rec_idx:
+        recvec.append(statemonitor[ri].v)
+    recvec = np.asarray(recvec)
+    np.save("recording", recvec)
+
--- tests/test_recordings.py
+++ tests/test_recordings.py
@@ -9,60 +9,57 @@ import matplotlib.pyplot as plt
 
 plain_numbers_from_list = lambda x: str(x)[1:-1].replace(',','')
 
+JNML_PATH = "C:/jNeuroMLJar"
+xml_filename = "ifcgmtest.xml"
+idx_to_record = [2, 55, 98]
+output_jnml_file = "recording_ifcgmtest"
+plot = False
+
 class RecordingsTest(unittest.TestCase):
     "test of LEMSExporter"
 
     def test_one(self):
-        pass
+        """
+        Test example 1: simulation1_lif.py
+        """
+        script = "simulation1_lif.py"
+        current_path = os.getcwd()
+        outcommand = os.popen("python {file} -d --recidx {recidx}".format(file=script,
+                                                                          recidx=plain_numbers_from_list(idx_to_record)))
+        outcommand = call("python {file} --recidx {recidx}".format(file=script,
+                                                                   recidx=plain_numbers_from_list(idx_to_record)),
+                          shell=True)
 
-JNML_PATH = "C:/jNeuroMLJar"
-xml_filename = "ifcgmtest.xml"
-script = "simulation1_lif.py"
-idx_to_record = [2, 55, 98]
-output_jnml_file = "recording_ifcgmtest"
+        outbrian = np.load(os.path.join(os.path.dirname(__file__), "recording.npy"))
+        #print outbrian
+        os.chdir(JNML_PATH)
+        outcommand = call("jnml {path} -nogui".format(path=os.path.join(current_path, xml_filename)),
+                          shell=True)
+        #print outcommand
 
-current_path = os.getcwd()
+        timevec = []
+        valuesvec = []
+        with open(output_jnml_file+'.dat','r') as f:
+            for line in f:
+                timevec.append(float(line.split("\t")[0]))
+                valuesvec.append([float(x) for x in line.split("\t")[1:-1]])
 
-outcommand = os.popen("python {file} -d --recidx {recidx}".format(file=script,
-                                                                  recidx=plain_numbers_from_list(idx_to_record)))
-
-outcommand = call("python {file} --recidx {recidx}".format(file=script,
-                                                           recidx=plain_numbers_from_list(idx_to_record)),
-                  shell=True)
-
-outbrian = np.load("recording.npy")
-#print outbrian
-os.chdir(JNML_PATH)
-outcommand = call("jnml {path} -nogui".format(path=os.path.join(current_path, xml_filename)),
-                  shell=True)
-#print outcommand
-
-timevec = []
-valuesvec = []
-with open(output_jnml_file+'.dat','r') as f:
-    for line in f:
-        timevec.append(float(line.split("\t")[0]))
-        valuesvec.append([float(x) for x in line.split("\t")[1:-1]])
-
-timevec = np.asarray(timevec)
-valuesvec = np.asarray(valuesvec).T
-print outbrian[1, 0:10]
-print valuesvec[1, 1:10]
-print np.allclose(outbrian[1, 0:10], valuesvec[1, 1:11])
-plt.subplot(3,2,1)
-plt.plot(outbrian[0,:])
-plt.subplot(3,2,2)
-plt.plot(valuesvec[0,:])
-plt.subplot(3,2,3)
-plt.plot(outbrian[1,:])
-plt.subplot(3,2,4)
-plt.plot(valuesvec[1,:])
-plt.subplot(3,2,5)
-plt.plot(outbrian[2,:])
-plt.subplot(3,2,6)
-plt.plot(valuesvec[2,:])
-plt.show()
-
-os.chdir(current_path)
-#call(["python", "{file}".format(file=script)], shell=True)
-
+        timevec = np.asarray(timevec)
+        valuesvec = np.asarray(valuesvec).T
+        for i in range(len(idx_to_record)):
+            self.assertTrue(np.allclose(outbrian[i, :], valuesvec[i, 1:], atol=1e-02))
+        if plot:
+            plt.subplot(3,2,1)
+            plt.plot(outbrian[0,:])
+            plt.subplot(3,2,2)
+            plt.plot(valuesvec[0,:])
+            plt.subplot(3,2,3)
+            plt.plot(outbrian[1,:])
+            plt.subplot(3,2,4)
+            plt.plot(valuesvec[1,:])
+            plt.subplot(3,2,5)
+            plt.plot(outbrian[2,:])
+            plt.subplot(3,2,6)
+            plt.plot(valuesvec[2,:])
+            plt.show()
+        os.chdir(current_path)
